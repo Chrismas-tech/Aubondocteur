2fa97e6b9fa2f1161404d1292cc686b288f52e11
Chris <lucianichristophe0@gmail.com>
2021-02-14 21:42:31 +0100

    st correctif

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -13,6 +13,7 @@
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
+                
 
                     {{ __('Before proceeding, please check your email for a verification link.') }}
                     {{ __('If you did not receive the email') }},
