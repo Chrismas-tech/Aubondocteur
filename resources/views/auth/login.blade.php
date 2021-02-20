8b361752f65faf99b17e45675ad57aab7e8c17c6
Chris <lucianichristophe0@gmail.com>
2021-02-20 14:47:37 +0100

    eye mot de passe style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-    <div class="my_min_height_login mq_h1_font_size jumbotron text-primary bg_photo_3 mb-0 d-lg-flex justify-content-around">
+    <div
+        class="my_min_height_login mq_h1_font_size jumbotron text-primary bg_photo_3 mb-0 d-lg-flex justify-content-around">
         <div class="bg-white mt-xl-3 mb-5 mb-lg-0 p-1 mb-xl-0 rounded col-12 col-lg-6">
 
             <div class="text-center mt-2">
@@ -55,29 +56,33 @@
                     <div class="">
                         <label for="password" class="col-md-4 col-form-label text-md-right"></label>
 
-                        <div class="input-group-lg">
+                        <div class="input-group-lg d-flex">
                             <input id="password" type="password"
-                                class="form-control @error('password') is-invalid @enderror" name="password" required
-                                autocomplete="new-password" placeholder="Votre mot de passe">
+                                class="input_rounded_right_0 form-control  @error('password') is-invalid @enderror" name="password"
+                                required autocomplete="new-password" placeholder="Votre mot de passe">
+                            <button id="reveal_password_register" class="border border-secondary "><img src="{{ asset('img/eye.png') }}" alt="" class="eye_icon"></button>
 
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
                         </div>
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
                     <div class="">
                         <label for="password-confirm" class="col-md-4 col-form-label text-md-right"></label>
 
-                        <div class="input-group-lg">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                        <div class="input-group-lg d-flex">
+                            <input id="password-confirm" type="password" class="input_rounded_right_0 form-control" name="password_confirmation"
                                 required autocomplete="new-password" placeholder="Confirmation du mot de passe">
+                                <button id="reveal_password_confirmation_register" class="border border-secondary "><img src="{{ asset('img/eye.png') }}" alt="" class="eye_icon"></button>
                         </div>
                     </div>
                     <div>
                         <div class="text-center mt-4 mb-4 mt-lg-5 mb-lg-5">
-                            <button type="submit" class="btn_login_register_font_size btn btn-primary btn-lg p_texte_1 text-white">
+                            <button type="submit"
+                                class="btn_login_register_font_size btn btn-primary btn-lg p_texte_1 text-white">
                                 S'enregistrer
                             </button>
                         </div>
@@ -111,7 +116,7 @@
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                 name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
                                 placeholder="Votre Email">
-
+                                
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -120,13 +125,14 @@
                         </div>
                     </div>
 
-                    <div  class="mb-3">
+                    <div class="mb-3">
                         <label for="password" class="col-form-label"></label>
 
-                        <div class="input-group-lg">
+                        <div class="input-group-lg d-flex">
                             <input id="password" type="password"
-                                class="form-control @error('password') is-invalid @enderror" name="password" required
+                                class="input_rounded_right_0 form-control @error('password') is-invalid @enderror" name="password" required
                                 autocomplete="current-password" placeholder="Votre mot de passe">
+                                <button id="reveal_password_login" class="border border-secondary"><img src="{{ asset('img/eye.png') }}" alt="" class="eye_icon"></button>
 
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -148,13 +154,14 @@
                         </div>
 
                         <div class="mt-4 mb-4 mt-lg-5 mb-lg-5">
-                            <button type="submit" class="btn_login_register_font_size btn btn-primary btn-lg p_texte_1 text-white">
+                            <button type="submit"
+                                class="btn_login_register_font_size btn btn-primary btn-lg p_texte_1 text-white">
                                 Connexion
                             </button>
 
                             @if (Route::has('password.request'))
                                 <a class="btn btn-link btn-lg" href="{{ route('password.request') }}">
-                                   Forgot your password
+                                    Forgot your password
                                 </a>
                             @endif
                         </div>
